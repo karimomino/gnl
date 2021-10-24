b784255427d05ca33e02b9a8d85028b527034b5d
Karim Amin <kamin@lab1r1s8.42abudhabi.ae>
2021-10-24 22:15:50 +0400

    IDK Anymore

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kamin <kamin@42abudhabi.ae>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 03:38:23 by kamin             #+#    #+#             */
-/*   Updated: 2021/10/20 05:57:16 by kamin            ###   ########.fr       */
+/*   Updated: 2021/10/20 19:09:22 by kamin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ char	*ft_line(char **file_content)
 	i = 0;
 	if (*file_content == NULL || **file_content == '\0')
 		return (NULL);
-	while ((*file_content)[i] != '\0' && (*file_content)[i] != '\n')
+	while ((*file_content)[i] != '\0')
 		i++;
 	ret = ft_substr(*file_content, 0, i);
 	*file_content += i + 1;
@@ -51,36 +51,39 @@ char	*get_next_line(int fd)
 	char		*tmp;
 	static char	*file_content;
 	int			read_val;
-
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	read_val = -1;
-	while (ft_strchr(buff, '\n') == NULL && read_val != 0)
+	read_val = 1;
+	while (ft_strchr(buff, '\n') == NULL && read_val > 0)
 	{
 		read_val = read(fd, buff, BUFFER_SIZE);
+		if (read_val < 0)
+			return (NULL);
 		buff[read_val] = '\0';
 		if (file_content == NULL && read_val > 0)
 			file_content = ft_strdup(buff);
 		else if (read_val > 0)
 		{
 			tmp = ft_strjoin(file_content, buff);
-			//free(file_content);
+			free(file_content);
 			file_content = tmp;
+			free(tmp);
 		}
 	}
+
 	return (ft_line(&file_content));
 }
 
-int main()
-{
-	char *x;
-	int fd = open("empty_file", O_RDONLY);
-	x = get_next_line(fd);
-	//printf("%s", x);
-	while (x != NULL)
-	{
-		printf("%s\n", x);
-		x = get_next_line(fd);
-	}
-	return 0;
-}
+// int main()
+// {
+// 	char *x;
+// 	int fd = open("nl", O_RDONLY);
+// 	x = get_next_line(fd);
+// 	//printf("%s", x);
+// 	while (x != NULL)
+// 	{
+// 		printf("%s\n", x);
+// 		x = get_next_line(fd);
+// 	}
+// 	return 0;
+// }
