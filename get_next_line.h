55eb08aa9b9a7ad31167b3fdcd8140cc731e03e9
karimomino <kareemamin2005@gmail.com>
2021-10-25 00:37:22 +0000

    Fixed all Norms EXCEPT for Headers

--- get_next_line.h
+++ get_next_line.h
@@ -1,16 +1,8 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-/*DELETE BELOW*/
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 4
-#endif
-#include <fcntl.h>
-#include <stdio.h>
-#include <string.h>
-/*DELETE ABOVE*/
-#include <unistd.h>
- #include <stdlib.h>
+# include <unistd.h>
+# include <stdlib.h>
 
 char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
